0a0e2ca2eb44fda559b180d53b83f073b02862b3
LeoALXHIl <leoaleixohilario@gmail.com>
2025-03-06 11:27:02 -0300

    Add introductory JavaScript scripts for basic data types and operations

---
+++ IntroducaoJS/script00.js
@@ -0,0 +1,34 @@
+const texto ="Alô, Mundo!!!"
+var BoasVindas = "Bem-Vindos, Alunos de JS"
+BoasVindas = "Boas Vindas Alunos"
+
+console.log(texto);
+console.log(BoasVindas)
+
+
+// tipos de dados
+
+let string = "Variavel De Texto"; // sempre aspas duplas
+
+let numero1 = 42;//numero inteiro
+let numero2 = 10.65;//numero decimal
+let numero3 = "2";//texto 
+let boolean = true
+let boolean2 = false
+let boolean3 = "true"; // texto
+let undefined;
+let nula= mull;
+
+//let, Var e Const
+let resultado= true;
+resultado = false;
+//nao reescreva
+
+// reatribuiçao e reescrita
+var nota = 10;
+nota = 9;
+var nota = 8;
+
+const fixo =42;
+fixo =43;
+//nst fixo = 41= //er nao posso rescrever
---
+++ IntroducaoJS/script01operacoes.js
@@ -0,0 +1,33 @@
+let a = 10
+let b = 3
+console.log(a+b); 
+console.log("Subtração")
+console.log(a-b);
+console.log("multiplicação")
+console.log(a*b);
+console.log("Divisão")
+console.log(a/b);
+console.log("Resto da Divisão")
+console.log(a%b);
+
+//operadores comparacao
+
+//  (>,>=,<,<=,==,===)
+
+console.log("operadores relacionais")
+console.log(5>10)
+console.log(10 == "10");
+console.log(10 ==="10");
+
+// operadores logicos
+
+let nota1 = 5;
+let nota2 = 8;
+// our
+console.log(nota1>=7 || nota2>=7);
+
+// and
+console.log(nota1>=7&&nota2>=7);
+
+console.log(!true); //false
+
